4913525cf26b08017d2de4a9524880c7023aa26f
Gladislao <Gladislao@users.noreply.github.com>
2019-03-01 12:17:13 -0500

    Update simple_remove_duplicates.js

--- codewars/simple_remove_duplicates.js
+++ codewars/simple_remove_duplicates.js
@@ -12,31 +12,3 @@ function solve (arr) {
 }
 
 console.log(solve ([3, 4, 4, 3, 6, 3]))
-
-
-
-/*
-function isBigEnough(value) {
-    return value >= 10;
-  }
-  
-var filtered = [12, 5, 8, 130, 44].filter(isBigEnough); 
-console.log(filtered)
-
-
-/*
-if(result.indexOf(string[i]) == -1){ 
-            console.log(string[i])
-            result.push(string[i])
-        }
-
-function solve (arr) {
-    var result = []
-    size = arr.length
-    for (let i=0; i < size; i++){
-        
-    }
-    result = result.join(" ");
-    return result
-}
- */
