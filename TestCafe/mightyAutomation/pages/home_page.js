ed9f24034fd6872e8c59087ba1f3c212fadc5b56
Vicky Sanchez <victoria@able.co>
2019-04-22 17:55:26 -0500

    Remove wrong directory

--- TestCafe/mightyAutomation/pages/home_page.js
+++
@@ -1,9 +0,0 @@
-import {Selector} from 'testcafe';
-
-export default class HomePage {
-    constructor(){
-        this.dashboardLnk = Selector('a.u-tabDefault').withText('Dashboard');
-        this.addNewBtn = Selector('button').withText('Add New...');
-        this.newHoldingMenuItem = Selector('a.u-dropdownLink').withText('New Holding');
-    }
-}
